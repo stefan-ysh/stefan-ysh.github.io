e19a396f5c5a0aefeb8c36fee37d0166983b7987
苑帅 <58702199+stefan-ysh@users.noreply.github.com>
2021-11-19 16:53:51 +0800

    init blog

--- README.md
+++ README.md
@@ -1,13 +1,52 @@
-# Vno - Jekyll
+<h1>Flexton is an ultra-minimalist and responsive theme for Jekyll</h1>
 
-[Vno Jekyll](https://github.com/onevcat/vno-jekyll) is a theme for [Jekyll](http://jekyllrb.com). It is a port of my Ghost theme [vno](https://github.com/onevcat/vno), which is originally developed from [Dale Anthony's Uno](https://github.com/daleanthony/uno).
+<p>Flexton created especially for those who appreciate minimalism and functionality.</p>
+<p>Amazing flexibility with pleasant colors and custom design makes Flexton a simple theme, suitable for almost any blog.</p>
 
-## Live Demo
+<h2>Demo</h2>
 
-See [Vno - Jekyll](http://vno.onevcat.com) site and [my blog](http://onevcat.com).
+Check the theme in action [Demo](https://flexton.netlify.com/)
 
-You can also find some instruction as well as other sites using Vno theme in [this page](http://vno.onevcat.com/2016/02/hello-world-vno/).
+![Page preview](https://github.com/artemsheludko/flexton/blob/master/images/preview.png?raw=true)
 
-## Licence
+<h2>Features</h2>
 
-Great thanks to [Dale Anthony](https://github.com/daleanthony) and his [Uno](https://github.com/daleanthony/uno). Vno Jekyll is based on Uno, and contains a lot of modification on page layout, animation, font and some more things I can not remember. Vno Jekyll is followed with Uno and be licensed as [Creative Commons Attribution 4.0 International](http://creativecommons.org/licenses/by/4.0/). See the link for more information.
+<ul>
+	<li>100% responsive and clean theme</li>
+ 	<li>Optimized for mobile devices</li>
+	<li>Valid HTML5 code</li>
+	<li>Included site search</li>
+	<li>Contact Page</li>
+	<li>Post sharing</li>
+	<li>Supports Mail Chimp Subscriber</li>
+	<li>Supports Disqus Comments</li>
+	<li>Social Media Profiles</li>
+	<li>Contact Form - FormsPree</li>
+	<li>Evil Icons</li>
+	<li>Google Fonts</li>
+</ul>
+
+<h2>Credits</h2>
+
+<p>I have used the following scripts, fonts or other files as listed.</p>
+
+<ul>
+  <li><a href="https://fonts.google.com/">Google Fonts</a> (Volkhov, Open Sans).</li>
+  <li><a href="http://evil-icons.io/">Evil Icons</a></li>
+  <li><a href="http://fitvidsjs.com/">FitVids.js</a></li>
+  <li><a href="https://jquery.com/">jQuery.com</a></li>
+  <li><a href="https://github.com/christian-fei/Simple-Jekyll-Search">Simple-Jekyll-Search</a></li>
+  <li>Preview Images form <a href="https://unsplash.com/">unsplash.com</a>, <a href="https://www.pexels.com/">pexels.com</a></li>
+</ul>
+
+<h2>Deployment</h2>
+
+To run the theme locally, navigate to the theme directory and run `bundle install` to install the dependencies, then run `jekyll serve` or `bundle exec serve` to start the Jekyll server.
+
+I would recommend checking the [Deployment Methods](https://jekyllrb.com/docs/deployment-methods/) page on Jekyll website.
+
+<h2>Buy me a coffee</h2>
+
+<p>If you want to show your appreciation, buy me one <a href="https://www.buymeacoffee.com/artemsheludko" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: auto !important;width: auto !important;" ></a> ! Every five cups of coffee and a new theme for Jekyll is ready 😋</p>
+<p>Either way, your support is a way to thank me ❤️</p>
+<p align="center"><b>Thank you for your support!</b></p>
--- js/main.js
+++ js/main.js
@@ -1,52 +1,103 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  $('a.blog-button').click(function() {
-    // If already in blog, return early without animate overlay panel again.
-    if (location.hash && location.hash == "#blog") return;
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
-    $('.main-post-list').removeClass('hidden');
-    currentWidth = $('.panel-cover').width();
-    if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed');
+  'use strict';
+
+  /* =======================
+  // Simple Search Settings
+  ======================= */
+
+  SimpleJekyllSearch({
+    searchInput: document.getElementById('js-search-input'),
+    resultsContainer: document.getElementById('js-results-container'),
+    json: '/search.json',
+    searchResultTemplate: '<li><a href="{url}">{title}</a></li>',
+    noResultsText: '<li>No results found</li>'
+  })
+
+  /* =======================
+  // Responsive videos
+  ======================= */
+
+  $('.c-wrap-content').fitVids({
+    'customSelector': ['iframe[src*="ted.com"]']
+  });
+
+  /* =======================================
+  // Switching between posts and categories
+  ======================================= */
+
+  $('.c-nav__list > .c-nav__item').click(function() {
+    $('.c-nav__list > .c-nav__item').removeClass('is-active');
+    $(this).addClass('is-active');
+    if ($('.c-nav__item:last-child').hasClass('is-active')) {
+      $('.c-posts').css('display', 'none').removeClass('o-opacity');
+      $('.c-load-more').css('display', 'none')
+      $('.c-categories').css('display', '').addClass('o-opacity');
     } else {
-      $('.panel-cover').css('max-width',currentWidth);
-      $('.panel-cover').animate({'max-width': '700px', 'width': '30%'}, 400, swing = 'swing', function() {} );
+      $('.c-posts').css('display', '').addClass('o-opacity');
+      $('.c-load-more').css('display', '')
+      $('.c-categories').css('display', 'none').removeClass('o-opacity');
     }
   });
 
-  if (window.location.hash && window.location.hash == "#blog") {
-    $('.panel-cover').addClass('panel-cover--collapsed');
-    $('.main-post-list').removeClass('hidden');
+  /* =======================
+  // Adding ajax pagination
+  ======================= */
+
+  $(".c-load-more").click(loadMorePosts);
+
+  function loadMorePosts() {
+    var _this = this;
+    var $postsContainer = $('.c-posts');
+    var nextPage = parseInt($postsContainer.attr('data-page')) + 1;
+    var totalPages = parseInt($postsContainer.attr('data-totalPages'));
+
+    $(this).addClass('is-loading').text("Loading...");
+
+    $.get('/page/' + nextPage, function (data) {
+      var htmlData = $.parseHTML(data);
+      var $articles = $(htmlData).find('article');
+
+      $postsContainer.attr('data-page', nextPage).append($articles);
+
+      if ($postsContainer.attr('data-totalPages') == nextPage) {
+        $('.c-load-more').remove();
+      }
+
+      $(_this).removeClass('is-loading');
+    });
   }
 
-  if (window.location.pathname.substring(0, 5) == "/tag/") {
-    $('.panel-cover').addClass('panel-cover--collapsed');
-  }
+  /* ==============================
+  // Smooth scroll to the tags page
+  ============================== */
 
-  $('.btn-mobile-menu__icon').click(function() {
-    if ($('.navigation-wrapper').css('display') == "block") {
-      $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-        $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
-        $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-      });
-      $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
+  $('.c-tag__list a').on('click', function (e) {
+    e.preventDefault();
 
+    var currentTag = $(this).attr('href'),
+      currentTagOffset = $(currentTag).offset().top;
+
+    $('html, body').animate({
+      scrollTop: currentTagOffset - 10
+    }, 400);
+
+  });
+
+  /* =======================
+  // Scroll to top
+  ======================= */
+
+  $('.c-top').click(function () {
+    $('html, body').stop().animate({ scrollTop: 0 }, 'slow', 'swing');
+  });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > $(window).height()) {
+      $('.c-top').addClass("c-top--active");
     } else {
-      $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
-    }
-    $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
+      $('.c-top').removeClass("c-top--active");
+    };
   });
 
-  $('.navigation-wrapper .blog-button').click(function() {
-    if ($('.navigation-wrapper').css('display') == "block") {
-      $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-        $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
-        $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-      });
 
-      $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
-    }
-    
-    $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
-  });
 });
